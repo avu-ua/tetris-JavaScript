fd65256ef937b6d5624942aa33a622b3a81b5785
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-08-17 21:07:58 +0200

    JS ffile reworked. Needs debugging. CheckBeforeRendering() in progress.

--- js/index.js
+++ js/index.js
@@ -10,114 +10,136 @@ function test() {
     cellID = rrow + "-" + ccol;
 }
 
-/*
-function Block() {
-    this.l1c1 = "c1-4"
-    this.l1c2 = "c1-5"
-    this.l1c3 = "c1-6"
-    this.l2c1 = "c2-4"
-    this.l2c2 = "c2-5"
-    this.l2c3 = "c2-6"
-    this.l3c1 = "c3-4"
-    this.l3c2 = "c3-5"
-    this.l3c3 = "c3-6"
 
-    this.l1c1_color = "blue"
-    this.l1c2_color = "c1-5"
-    this.l1c3_color = "c1-6"
-    this.l2c1_color = "c2-4"
-    this.l2c2_color = "c2-5"
-    this.l2c3_color = "c2-6"
-    this.l3c1_color = "c3-4"
-    this.l3c2_color = "c3-5"
-    this.l3c3_color = "c3-6"
-
-    // this.l2c2.backgroundColor = "green"
-    // this.l3c1.backgroundColor = "green"
-    // this.l3c2.backgroundColor = "green"
-    // this.l3c3.backgroundColor = "green"
-}
-
-var t_block = new Block()
-t_block.backgroundColor = "green"
-t_block.backgroundColor = "green"
-t_block.backgroundColor = "green"
-t_block.backgroundColor = "green"
-
-var line_three = new Block()
-t_block.backgroundColor = "green"
-t_block.backgroundColor = "green"
-t_block.backgroundColor = "green"
-t_block.backgroundColor = "green"
-
-
-function rotateright() {
-    for (let r = 1; r < 4; r++) {
-        for (let c = 1; c < 4; c++) {
-            var target = document.getElementById(t_block)
-        }
-    }
-} */
 
 // get 'stanbdard' color of gamefield frm CSS variable
-const standardColor = getComputedStyle(document.body).getPropertyValue('--cell-color');
+const STANDARD_COLOR = getComputedStyle(document.body).getPropertyValue('--cell-color');
 
 // det color of blocks from CSS variable
-const blockColor = getComputedStyle(document.body).getPropertyValue('--block-color');
+const BLOCK_COLOR = getComputedStyle(document.body).getPropertyValue('--block-color');
 
-// define blocks (tBlock, lineThree, lineTwo, singleCell, smallCorner, largeCorner,
-// square2x2, dollarRight, dollarLeft)
-const tBlock = [1, 1, 1, 0, 1, 0, 0, 0, 0];
-const lineThree = [0, 0, 0, 1, 1, 1, 0, 0, 0];
-const lineTwo = [1, 1, 0, 0];
-const singleCell = [1];
-const smallCorner = [1, 1, 0, 1];
-const largeCorner = [1, 1, 1, 1, 0, 0, 1, 0, 0];
-const square2x2 = [1, 1, 1, 1];
-const dollarRight = [0, 0, 0, 0, 1, 1, 1, 1, 0];
-const dollarLeft = [0, 0, 0, 1, 1, 0, 0, 1, 1];
+// will be switched on at start of game and switched off when game is over
+let gameOver = true;
 
-// temporary. newElem selection should be randomised
-let newElem = singleCell;
-
+// create object Block
 class Block {
     constructor(elem) {
         if (elem.length === 9) {
-            this.idArray = ["1-4", "1-5", "1-6", "2-4", "2-5", "2-6", "3-4", "3-5", "3-6"];
+            // this.idArray = ["1-4", "1-5", "1-6", "2-4", "2-5", "2-6", "3-4", "3-5", "3-6"];
+            this.idArray = [[1,4], [1,5], [1,6], [2,4], [2,5], [2,6], [3,4], [3,5], [3,6]]
         } else if (elem.length === 4) {
-            this.idArray = ["1-5", "1-6", "2-5", "2-6"];
+            // this.idArray = ["1-5", "1-6", "2-5", "2-6"];
+            this.idArray = [[1,5], [1,6], [2,5], [2,6]]
         } else {
-            this.idArray = ["1-5"];
+            // this.idArray = ["1-5"];
+            this.idArray = [[1,5]];
         }
         this.colors = elem;
+        this.newborn = true;
     }
 }
+
+// define blocks (T_SHAPE, LINE_THREE, LINE_TWO, SINGLE_CELL, SMALL_CORNER, LARGE_CORNER,
+// SQUARE2x2, DOLLAR_RIGHT, DOLLAR_LEFT).
+const T_SHAPE = [0, 0, 0, 1, 1, 1, 0, 1, 0];
+const LINE_THREE = [0, 0, 0, 1, 1, 1, 0, 0, 0];
+const LINE_TWO = [1, 1, 0, 0];
+const SINGLE_CELL = [1];
+const SMALL_CORNER = [1, 1, 0, 1];
+const LARGE_CORNER = [1, 1, 1, 1, 0, 0, 1, 0, 0];
+const SQUARE2x2 = [1, 1, 1, 1];
+const DOLLAR_RIGHT = [0, 0, 0, 0, 1, 1, 1, 1, 0];
+const DOLLAR_LEFT = [0, 0, 0, 1, 1, 0, 0, 1, 1];
+
+// create Array that indicates if a cell is already occupied.
+// Value 0 will mean 'free', 1 - 'occupied by moving block', 2 - 'occupied by previous block(s)'.
+let occupiedArray = [];
+for (let i = 0; i < 25; i++) {
+    let clipboard = [];
+    for (let j = 0; j < 10; j++) {
+        clipboard.push(0);
+        // this section is temporary for development purposes. Delete later
+        let id = (i + 1).toString() + "-" + (j + 1).toString();
+        let tempCell = document.getElementById(id);
+        tempCell.innerHTML = "0";
+        tempCell.style.display = "flex";
+        tempCell.style.alignItems = "center";
+        tempCell.style.justifyContent = "center";
+        tempCell.style.color = "red";
+        tempCell.style.fontWeight = "700";
+        tempCell.style.fontSize = "12px";
+        tempCell.style.width = "40px";
+        tempCell.style.height = "40px";
+        // end of temporary section
+    }
+    occupiedArray.push(clipboard);
+}
+
+// this section is temporary for development purposes. Delete later
+function temporaryShowInner() {
+    let id;
+    let tempIndex;
+    let tempCell;
+    for (let i = 0; i < 25; i++) {
+        for (let j = 0; j < 10; j++) {
+            id = (i + 1).toString() + "-" + (j + 1).toString();
+            tempIndex = occupiedArray[i][j].toString();
+            tempCell = document.getElementById(id);
+            tempCell.innerHTML = tempIndex;
+        }
+    }
+    
+}
+// end of temporary section
+
+// temporary. newElem selection should be randomised
+let newElem = T_SHAPE;
 
 // initial place of block on gamefield
 let currentBlock = new Block(newElem);
 render(currentBlock);
 
-// function to render (re-render) block on gamefield
-function render(block) {
-    for (let i = 0; i < block.idArray.length; i++) {
-        let cell = document.getElementById(block.idArray[i]);
-        if (block.colors[i] === 1) {
-            cell.style.backgroundColor = blockColor;
-        } else {
-            cell.style.backgroundColor = standardColor;
+// function to check if able to render.
+// Returns 1 if ok to render, 0 if rendering should be skipped, 
+// and 2 if rendering is not possible AND the block is 'newborn' - in this case game over
+function checkBeforeRendering(plannedBlock) {
+    for (let i = 0; i < plannedBlock.idArray.length; i++) {
+        if (occupiedArray[plannedBlock.idArray[i][0] - 1][plannedBlock.idArray[i][1] - 1] === 2) {
+            occupiedArray[plannedBlock.idArray[i][0] - 1][plannedBlock.idArray[i][1] - 1] = plannedBlock.colors[i];
         }
     }
+
 }
 
-// rotate block counter-clockwise
-function rotateleft() {
-    let copy = currentBlock.colors;
-    if (currentBlock.idArray.length === 9) {
-        currentBlock.colors = [copy[2], copy[5], copy[8], copy[1], copy[4], copy[7], copy[0], copy[3], copy[6]];
-    } else if (currentBlock.idArray.length === 4) {
-        currentBlock.colors = [copy[1], copy[3], copy[0], copy[2]];
+// function to render (re-render) block on gamefield
+function render(block) {
+    let id;
+    let cell;
+    for (let i = 0; i < block.idArray.length; i++) {
+        id = block.idArray[i][0].toString() + "-" + block.idArray[i][1].toString();
+            cell = document.getElementById(id);
+        if (block.colors[i]) {
+            cell.style.backgroundColor = BLOCK_COLOR;
+        } else if (occupiedArray[block.idArray[i][0] - 1][block.idArray[i][1] - 1] !== 2) {
+            cell.style.backgroundColor = STANDARD_COLOR;
+        }
     }
-    render(currentBlock);
+    // this section is temporary for development purposes. Delete later
+    temporaryShowInner();
+    // end of temporary section
+}
+
+// function to mark cells on gamefield as 'feee', 'occupied' or 'occupied forever' on move of a Block
+function markOccupiedOrFree(block) {
+    for (let i = 0; i < block.idArray.length; i++) {
+        if (occupiedArray[block.idArray[i][0] - 1][block.idArray[i][1] - 1] !== 2) {
+            occupiedArray[block.idArray[i][0] - 1][block.idArray[i][1] - 1] = block.colors[i];
+        }
+    }
+    // this section is temporary for development purposes. Delete later
+    temporaryShowInner();
+    // end of temporary section
+    
 }
 
 // rotate block clockwise
@@ -128,42 +150,71 @@ function rotateright() {
     } else if (currentBlock.idArray.length === 4) {
         currentBlock.colors = [copy[2], copy[0], copy[3], copy[1]];
     }
+    let checkResult = checkBeforeRendering(currentBlock);
+    if (!checkResult) {
+        currentBlock.colors = copy;
+        return;
+    }
     render(currentBlock);
+    markOccupiedOrFree(currentBlock);
+    // console.log(occupiedArray);
+}
+
+// rotate block counter-clockwise
+function rotateleft() {
+    let copy = currentBlock.colors;
+    if (currentBlock.idArray.length === 9) {
+        currentBlock.colors = [copy[2], copy[5], copy[8], copy[1], copy[4], copy[7], copy[0], copy[3], copy[6]];
+    } else if (currentBlock.idArray.length === 4) {
+        currentBlock.colors = [copy[1], copy[3], copy[0], copy[2]];
+    }
+    let checkResult = checkBeforeRendering(currentBlock);
+    if (!checkResult) {
+        currentBlock.colors = copy;
+        return;
+    }
+    render(currentBlock);
+    markOccupiedOrFree(currentBlock);
+    // console.log(occupiedArray);
 }
 
 // moving block left
 function moveleft() {
-    shiftLeftOrRight(-1);
-    // if (currentBlock.idArray[0][2] > 1) {
-    //     for (let i = 0; i < currentBlock.idArray.length; i++) {
-    //         let newStartColumn = currentBlock.idArray[i][2] - 1;
-    //         currentBlock.idArray[i] = currentBlock.idArray[0];
-    //         currentBlock.idArray[i][2] -= 1;
-    //         console.log(currentBlock.idArray[i]);
-    //         console.log(currentBlock.idArray[i][2] - 1);
-    //     }
-    // }
-    // render(currentBlock);
+    prepareToShiftHorizontal(-1);
+    render(currentBlock);
 }
 
-function shiftLeftOrRight(direction) {
+// moving block right
+function moveleft() {
+    prepareToShiftHorizontal(1);
+    render(currentBlock);
+}
+
+function prepareToShiftHorizontal(direction) {
+    let copyBlock = currentBlock;
+    currentBlock.colors = copyBlock.colors;
     for (let i = 0; i < currentBlock.idArray.length; i++) {
-        let idString = currentBlock.idArray[i];
-        let currentColumn;
-        if (idString[idString.length - 2] === "-") {
-            currentColumn = Number(idString[idString.length - 1]);
-            let newColumn = (currentColumn + direction).toString();
-            console.log("newColumn", newColumn);
-            let newID = idString.replace(idString[-1], newColumn);
-            console.log("newID]", newID);
-            currentBlock.idArray[i] = newID;
-            console.log("currentBlock.idArray[i]", currentBlock.idArray[i]);
-        } else {
-            currentColumn = Number(idString.substr(-2, 2));
-            let newColumn = currentColumn + direction;
-            let newID = idString.replace(idString[-2, 2], newColumn);
-            currentBlock.idArray[i] = newID;
-            console.log(currentBlock.idArray[i]);
+        currentBlock.idArray[i][0] += direction;
+        if (occupiedArray[currentBlock.idArray[i][0] - 1 + direction][currentBlock.idArray[i][1] - 1] === 2
+            || currentBlock.idArray[i][0] < 1
+            || currentBlock.idArray[i][0] > 10) {
+            currentBlock = copyBlock;
+            return;
+        }            
+    }
+    // make good those cells from which the current block departs
+    let beingLeftBehind = [];
+    if (direction < 0 && copyBlock.idArray[2][1] < 10) {
+        beingLeftBehind = [copyBlock.idArray[2], copyBlock.idArray[5], copyBlock.idArray[8]];
+    } else if (direction > 0 && copyBlock.idArray[0][1] > 1) {
+        beingLeftBehind = [copyBlock.idArray[0], copyBlock.idArray[3], copyBlock.idArray[6]];
+    }
+    for (let j = 0; j < 3; j++) {
+        if (occupiedArray[beingLeftBehind[0] - 1, beingLeftBehind[1] - 1] !== 2) {
+            occupiedArray[beingLeftBehind[0] - 1, beingLeftBehind[1] - 1] = 0;
+            let id = beingLeftBehind[0].toString() + "-" + beingLeftBehind[1].toString();
+            console.log(beingLeftBehind);
+            document.getElementById(id).style.backgroundColor = STANDARD_COLOR;
         }
     }
 }
