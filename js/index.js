8bd942fd6dccb90373e07daa3310eb72325c7250
Vyacheslav Anishchenko <avuua16@gmail.com>
2023-08-19 22:34:31 +0200

    T-Shape now can be moved right-down-left, and rotated

--- js/index.js
+++ js/index.js
@@ -1,16 +1,3 @@
-/* THIS IS FOR TEST BUTTON. REMOVE LATER
-let rrow = 24
-let ccol = 0
-let cellID = rrow + '-' + ccol
-
-function test() {
-  var targetCell = document.getElementById(cellID)
-  targetCell.style.backgroundColor = 'green'
-  console.log("'Test' button was clicked. Click number ", ccol)
-  ccol = ccol + 1
-  cellID = rrow + '-' + ccol
-} */
-
 // get 'stanbdard' color of gamefield frm CSS variable
 const STANDARD_COLOR = getComputedStyle(document.body).getPropertyValue(
   '--cell-color'
@@ -21,178 +8,33 @@ const BLOCK_COLOR = getComputedStyle(document.body).getPropertyValue(
   '--block-color'
 )
 
-// will be switched on at start of game and switched off when game is over
-let gameOver = true
-
-// create object Block
-class Block {
-  constructor(elem) {
-    if (elem.length === 9) {
-      // this.idArray = ["1-4", "1-5", "1-6", "2-4", "2-5", "2-6", "3-4", "3-5", "3-6"];
-      this.idArray = [
-        [0, 4],
-        [0, 5],
-        [0, 6],
-        [1, 4],
-        [1, 5],
-        [1, 6],
-        [2, 4],
-        [2, 5],
-        [2, 6],
-      ]
-    } else if (elem.length === 4) {
-      // this.idArray = ["1-5", "1-6", "2-5", "2-6"];
-      this.idArray = [
-        [0, 5],
-        [0, 6],
-        [1, 5],
-        [1, 6],
-      ]
-    } else {
-      // this.idArray = ["1-5"];
-      this.idArray = [[0, 5]]
-    }
-    this.colors = elem
-    this.newborn = true
-  }
-}
-
-// define blocks (T_SHAPE, LINE_THREE, LINE_TWO, SINGLE_CELL, SMALL_CORNER, LARGE_CORNER,
-// SQUARE2x2, DOLLAR_RIGHT, DOLLAR_LEFT).
-const POSITION = ['normal', 'turned-right', 'upside-down', 'turned-left']
-
-const T_SHAPE = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-const LINE_THREE = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const LINE_TWO = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const SINGLE_CELL = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const SMALL_CORNER = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const LARGE_CORNER = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const SQUARE2x2 = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const DOLLAR_RIGHT = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-const DOLLAR_LEFT = {
-  currentPosition: 'normal',
-  positionsSet: {
-    normal: [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-right': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'upside-down': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-    'turned-left': [0, 0, 0, 0, 0, 0, 0, 0, 0],
-  },
-}
-
-
-
-
-
-// const T_SHAPE = [0, 0, 0, 1, 1, 1, 0, 1, 0]
-// const LINE_THREE = [0, 0, 0, 1, 1, 1, 0, 0, 0]
-// const LINE_TWO = [1, 1, 0, 0]
-// const SINGLE_CELL = [1]
-// const SMALL_CORNER = [1, 1, 0, 1]
-// const LARGE_CORNER = [1, 1, 1, 1, 0, 0, 1, 0, 0]
-// const SQUARE2x2 = [1, 1, 1, 1]
-// const DOLLAR_RIGHT = [0, 0, 0, 0, 1, 1, 1, 1, 0]
-// const DOLLAR_LEFT = [0, 0, 0, 1, 1, 0, 0, 1, 1]
-
-// create Array that indicates if a cell is already occupied.
-// Value 0 will mean 'free', 1 - 'occupied by moving block', 2 - 'occupied by previous block(s)'.
+// prepare array indicating occupied cells
+// ('0' - free, '1' - occupied by current block, '2' occupied "permantly")
 let occupiedArray = []
 for (let i = 0; i < 25; i++) {
   let clipboard = []
   for (let j = 0; j < 10; j++) {
     clipboard.push(0)
-    // this section is temporary for development purposes. Delete later
-    let tempCell = document.getElementById(getID(i, j))
-    tempCell.innerHTML = '0'
-    tempCell.style.display = 'flex'
-    tempCell.style.alignItems = 'center'
-    tempCell.style.justifyContent = 'center'
-    tempCell.style.color = 'red'
-    tempCell.style.fontWeight = '700'
-    tempCell.style.fontSize = '12px'
-    tempCell.style.width = '40px'
-    tempCell.style.height = '40px'
-    // end of temporary section
   }
   occupiedArray.push(clipboard)
 }
 
-// function to get an ID of a cell
-function getID(r, c) {
-  return r.toString() + '-' + c.toString()
+// this section is temporary for development purposes. Delete later
+for (let i = 0; i < 25; i++) {
+  for (let j = 0; j < 10; j++) {
+    let cell = document.getElementById(getID(i, j))
+    cell.innerHTML = occupiedArray[i][j]
+    cell.style.display = 'flex'
+    cell.style.alignItems = 'center'
+    cell.style.justifyContent = 'center'
+    cell.style.color = 'red'
+    cell.style.fontWeight = '700'
+    cell.style.fontSize = '12px'
+    cell.style.width = '40px'
+    cell.style.height = '40px'
+  }
 }
 
-// this section is temporary for development purposes. Delete later
 function temporaryShowInner() {
   for (let i = 0; i < 25; i++) {
     for (let j = 0; j < 10; j++) {
@@ -201,162 +43,151 @@ function temporaryShowInner() {
     }
   }
 }
+temporaryShowInner()
 // end of temporary section
 
-// temporary. newElem selection should be randomised
-let newElem = LINE_TWO
+// function to get an ID of a cell
+function getID(r, c) {
+  return r.toString() + '-' + c.toString()
+}
 
-// initial place of block on gamefield
-let currentBlock = new Block(newElem)
-render(currentBlock)
+// function to get a key of a dict by the key's value
+function getKey(dict, value) {
+  return Object.keys(dict).find(key => dict[key] === value)
+}
 
-// function to check if able to render.
-function checkBeforeRendering(plannedBlock) {
-  for (let i = 0; i < plannedBlock.idArray.length; i++) {
+const ORIENTATION = ['normal', 'turnedRight', 'upsideDown', 'turnedLeft']
+
+const T_SHAPE = {
+  'currentOrientation': 'normal',
+  'center': [1, 5],
+  'normal': {
+    'bricks': [[0, 0], [-1, 0], [0, 1], [0, -1]],
+    'ifRotateLeft': [[-1, -1], [1, -1], [-1, 1]],
+    'ifRotateRight': [[-1, -1], [-1, 1], [1, 1]],
+  },
+  'turnedRight': {
+    'bricks': [[0, 0], [-1, 0], [0, 1], [1, 0]],
+    'ifRotateLeft': [[-1, -1], [-1, 1], [1, 1]],
+    'ifRotateRight': [[-1, 1], [1, 1], [1, -1]],
+
+  },
+  'upsideDown': {
+    'bricks': [[0, 0], [0, 1], [1, 0], [0, -1]],
+    'ifRotateLeft': [[-1, 1], [1, 1], [1, -1]],
+    'ifRotateRight': [[-1, -1], [1, 1], [1, -1]],
+
+  },
+  'turnedLeft': {
+    'bricks': [[0, 0], [-1, 0], [1, 0], [0, -1]],
+    'ifRotateLeft': [[-1, -1], [1, 1], [1, -1]],
+    'ifRotateRight': [[-1, -1], [-1, 1], [1, -1]],
+  },
+}
+
+// initiate currentBlock.
+// Its selection from array of block-templates to be randomized later
+let currentBlock = T_SHAPE
+
+if (checkIfCanRender(currentBlock, 0)) {
+  render('paint', currentBlock)
+} else {
+  console.log("Game over!") // end game here if no more new block can be renderred
+}
+
+// As regards 'rotation: '0' - no rotation, '1' - right / clockwise, '-1' = left /counter-clockwise
+function checkIfCanRender(plannedBlock, rotation) {
+  let placementArea = plannedBlock[plannedBlock['currentOrientation']]['bricks']
+  for (let i = 0; i < placementArea.length; i++) {
+    let brickPlace = [plannedBlock['center'][0] + placementArea[i][0], plannedBlock['center'][1] + placementArea[i][1]]
     if (
-      occupiedArray[plannedBlock.idArray[i][0]][plannedBlock.idArray[i][1]] ===
-      2
-    ) {
-      return false
+      occupiedArray[brickPlace[0], brickPlace[1]] === 2
+      || brickPlace[0] < 0
+      || brickPlace[0] > 24
+      || brickPlace[1] < 0
+      || brickPlace[1] > 9
+      )
+    return false
+  }
+  if (rotation) {
+    let index = ORIENTATION.indexOf(plannedBlock['currentOrientation'])
+    let previousOrientation
+    let potentialObstales
+    switch (rotation) {
+      case 1:
+        !index ? previousOrientation = ORIENTATION[3] : previousOrientation = ORIENTATION[index - 1]  
+        potentialObstales = plannedBlock[previousOrientation]['ifRotateRight']
+        break
+      case -1:
+      index === 3 ? previousOrientation = ORIENTATION[0] : previousOrientation = ORIENTATION[index + 1]
+        potentialObstales = plannedBlock[previousOrientation]['ifRotateLeft']
+        break
+      }
+    for (let j = 0; j < potentialObstales.length; j++) {
+      let row = plannedBlock['center'][0] + potentialObstales[j][0]
+      let col = plannedBlock['center'][1] + potentialObstales[j][1]
+      console.log("col: ", col)
+      if (occupiedArray[row][col] === 2) {
+        return false
+      }
     }
   }
   return true
 }
 
-// function to render (re-render) block on gamefield
-function render(block) {
-  let cell
-  for (let i = 0; i < block.idArray.length; i++) {
-    cell = document.getElementById(
-      getID(block.idArray[i][0], block.idArray[i][1])
-    )
-    if (block.colors[i]) {
-      cell.style.backgroundColor = BLOCK_COLOR
-    } else if (occupiedArray[block.idArray[i][0]][block.idArray[i][1]] !== 2) {
-      cell.style.backgroundColor = STANDARD_COLOR
-    }
+function render(paintOrClear, block) {
+  let paintColor
+  if (paintOrClear === 'paint') {
+    occupationIndex = 1
+    paintColor = BLOCK_COLOR
+  } else {
+    paintColor = STANDARD_COLOR
+    occupationIndex = 0
   }
-  // this section is temporary for development purposes. Delete later
-  temporaryShowInner()
-  // end of temporary section
+  let blockOrientation = block['currentOrientation']
+  let center = block['center']
+  let bricks = block[blockOrientation]['bricks']
+  for (let i = 0; i < bricks.length; i++) {
+    let brickPosition = [center[0] + bricks[i][0], center[1] + bricks[i][1]]
+    document.getElementById(getID(brickPosition[0], brickPosition[1])).style.backgroundColor = paintColor
+    occupiedArray[brickPosition[0], brickPosition[1]] = occupationIndex
+    document.getElementById(getID(brickPosition[0], brickPosition[1])).innerHTML = occupiedArray[brickPosition[0], brickPosition[1]]
+  }
 }
 
-// function to mark cells on gamefield as 'feee', 'occupied' or 'occupied forever' on move of a Block
-function markOccupiedOrFree(block) {
-  for (let i = 0; i < block.idArray.length; i++) {
-    if (occupiedArray[block.idArray[i][0]][block.idArray[i][1]] !== 2) {
-      occupiedArray[block.idArray[i][0]][block.idArray[i][1]] = block.colors[i]
-    }
+function move(leftOrRight, down) {
+  let newBlock = deepCopy(currentBlock)
+  newBlock['center'] = [currentBlock['center'][0] + down, currentBlock['center'][1] + leftOrRight]
+  let rotation = 0
+  if (checkIfCanRender(newBlock, rotation)) {
+    render('clear', currentBlock)
+    render('paint', newBlock)
+    currentBlock = deepCopy(newBlock)
   }
-  // this section is temporary for development purposes. Delete later
-  temporaryShowInner()
-  // end of temporary section
 }
 
-// rotate block clockwise
-function rotateright() {
-  let copy = currentBlock.colors
-  if (currentBlock.idArray.length === 9) {
-    currentBlock.colors = [
-      copy[6],
-      copy[3],
-      copy[0],
-      copy[7],
-      copy[4],
-      copy[1],
-      copy[8],
-      copy[5],
-      copy[2],
-    ]
-  } else if (currentBlock.idArray.length === 4) {
-    currentBlock.colors = [copy[2], copy[0], copy[3], copy[1]]
+function rotate(rotation) {
+  let newBlock = deepCopy(currentBlock)
+  if (rotation === -1 && !ORIENTATION.indexOf(currentBlock['currentOrientation'])) {
+    newBlock['currentOrientation'] = ORIENTATION[3]
+    console.log(newBlock['currentOrientation'])
+  } else if (rotation === -1) {
+    newBlock['currentOrientation'] = ORIENTATION[ORIENTATION.indexOf(currentBlock['currentOrientation']) - 1]
+  } else if (rotation === 1 && ORIENTATION.indexOf(currentBlock['currentOrientation']) === 3) {
+    newBlock['currentOrientation'] = ORIENTATION[0]
+  } else {
+    newBlock['currentOrientation'] = ORIENTATION[ORIENTATION.indexOf(currentBlock['currentOrientation']) + 1]
+    console.log(newBlock['currentOrientation'])
   }
-  if (!checkBeforeRendering(currentBlock)) {
-    currentBlock.colors = copy
-    return
+  if (checkIfCanRender(newBlock, rotation)) {
+    render('clear', currentBlock)
+    render('paint', newBlock)
+    currentBlock = deepCopy(newBlock)
   }
-  render(currentBlock)
-  markOccupiedOrFree(currentBlock)
-  // console.log(occupiedArray);
 }
 
-// rotate block counter-clockwise
-function rotateleft() {
-  let copy = currentBlock.colors
-  if (currentBlock.idArray.length === 9) {
-    currentBlock.colors = [
-      copy[2],
-      copy[5],
-      copy[8],
-      copy[1],
-      copy[4],
-      copy[7],
-      copy[0],
-      copy[3],
-      copy[6],
-    ]
-  } else if (currentBlock.idArray.length === 4) {
-    currentBlock.colors = [copy[1], copy[3], copy[0], copy[2]]
-  }
-  if (!checkBeforeRendering(currentBlock)) {
-    currentBlock.colors = copy
-    return
-  }
-  render(currentBlock)
-  markOccupiedOrFree(currentBlock)
-  // console.log(occupiedArray);
-}
-/*
-// moving block left
-function moveleft() {
-  prepareToShiftHorizontal(-1)
-  render(currentBlock)
-}
 
-// moving block right
-function moveleft() {
-  prepareToShiftHorizontal(1)
-  render(currentBlock)
+// create deep copy of an object (dict, array)
+function deepCopy(original) {
+  return JSON.parse(JSON.stringify(original))
 }
-
-function prepareToShiftHorizontal(direction) {
-  let copyBlock = currentBlock
-  currentBlock.colors = copyBlock.colors
-  for (let i = 0; i < currentBlock.idArray.length; i++) {
-    currentBlock.idArray[i][0] += direction
-    if (
-      occupiedArray[currentBlock.idArray[i][0] - 1 + direction][
-        currentBlock.idArray[i][1] - 1
-      ] === 2 ||
-      currentBlock.idArray[i][0] < 1 ||
-      currentBlock.idArray[i][0] > 10
-    ) {
-      currentBlock = copyBlock
-      return
-    }
-  }
-  // make good those cells from which the current block departs
-  let beingLeftBehind = []
-  if (direction < 0 && copyBlock.idArray[2][1] < 10) {
-    beingLeftBehind = [
-      copyBlock.idArray[2],
-      copyBlock.idArray[5],
-      copyBlock.idArray[8],
-    ]
-  } else if (direction > 0 && copyBlock.idArray[0][1] > 1) {
-    beingLeftBehind = [
-      copyBlock.idArray[0],
-      copyBlock.idArray[3],
-      copyBlock.idArray[6],
-    ]
-  }
-  for (let j = 0; j < 3; j++) {
-    if (occupiedArray[(beingLeftBehind[0] - 1, beingLeftBehind[1] - 1)] !== 2) {
-      occupiedArray[(beingLeftBehind[0] - 1, beingLeftBehind[1] - 1)] = 0
-      let id =
-        beingLeftBehind[0].toString() + '-' + beingLeftBehind[1].toString()
-      console.log(beingLeftBehind)
-      document.getElementById(id).style.backgroundColor = STANDARD_COLOR
-    }
-  }
-} */
---
+++ js/index_old.js
@@ -0,0 +1,301 @@
+/* THIS IS FOR TEST BUTTON. REMOVE LATER
+let rrow = 24
+let ccol = 0
+let cellID = rrow + '-' + ccol
+
+function test() {
+  var targetCell = document.getElementById(cellID)
+  targetCell.style.backgroundColor = 'green'
+  console.log("'Test' button was clicked. Click number ", ccol)
+  ccol = ccol + 1
+  cellID = rrow + '-' + ccol
+} */
+
+// get 'stanbdard' color of gamefield frm CSS variable
+const STANDARD_COLOR = getComputedStyle(document.body).getPropertyValue(
+  '--cell-color'
+)
+
+// det color of blocks from CSS variable
+const BLOCK_COLOR = getComputedStyle(document.body).getPropertyValue(
+  '--block-color'
+)
+
+// will be switched on at start of game and switched off when game is over
+let gameOver = true
+
+// create object Block
+class Block {
+  constructor(elem) {
+    if (elem.length === 9) {
+      // this.idArray = ["1-4", "1-5", "1-6", "2-4", "2-5", "2-6", "3-4", "3-5", "3-6"];
+      this.idArray = [
+        [0, 4],
+        [0, 5],
+        [0, 6],
+        [1, 4],
+        [1, 5],
+        [1, 6],
+        [2, 4],
+        [2, 5],
+        [2, 6],
+      ]
+    } else if (elem.length === 4) {
+      // this.idArray = ["1-5", "1-6", "2-5", "2-6"];
+      this.idArray = [
+        [0, 5],
+        [0, 6],
+        [1, 5],
+        [1, 6],
+      ]
+    } else {
+      // this.idArray = ["1-5"];
+      this.idArray = [[0, 5]]
+    }
+    this.colors = elem
+    this.newborn = true
+  }
+}
+
+// define blocks (T_SHAPE, LINE_THREE, LINE_TWO, SINGLE_CELL, SMALL_CORNER, LARGE_CORNER,
+// SQUARE2x2, DOLLAR_RIGHT, DOLLAR_LEFT).
+const POSITION = ['normal', 'turnedRight', 'upsideDown', 'turnedLeft']
+
+const T_SHAPE = {
+  currentPosition: 'normal',
+  center: [1, 5],
+  normal: {
+    bricks: [[0, 0], [-1, 0], [0, 1], [0, -1]],
+    ifRotateLeft: [[-1, -1], [1, -1], [-1, 1]],
+    ifRotateRight: [[-1, -1], [-1, 1], [1, 1]],
+  },
+  turnedRight: {
+    bricks: [[0, 0], [-1, 0], [0, 1], [1, 0]],
+    ifRotateLeft: [[-1, -1], [-1, 1], [1, 1]],
+    ifRotateRight: [[-1, 1], [1, 1], [1, -1]],
+
+  },
+  upsideDown: {
+    bricks: [[0, 0], [0, 1], [1, 0], [0, -1]],
+    ifRotateLeft: [[-1, 1], [1, 1], [1, -1]],
+    ifRotateRight: [[-1, -1], [1, 1], [1, -1]],
+
+  },
+  turnedLeft: {
+    bricks: [[0, 0], [-1, 0], [1, 0], [0], -1],
+    ifRotateLeft: [[-1, -1], [1, 1], [1, -1]],
+    ifRotateRight: [[-1, -1], [-1, 1], [1, -1]],
+  },
+}
+  
+
+
+
+
+
+// const T_SHAPE = [0, 0, 0, 1, 1, 1, 0, 1, 0]
+// const LINE_THREE = [0, 0, 0, 1, 1, 1, 0, 0, 0]
+// const LINE_TWO = [1, 1, 0, 0]
+// const SINGLE_CELL = [1]
+// const SMALL_CORNER = [1, 1, 0, 1]
+// const LARGE_CORNER = [1, 1, 1, 1, 0, 0, 1, 0, 0]
+// const SQUARE2x2 = [1, 1, 1, 1]
+// const DOLLAR_RIGHT = [0, 0, 0, 0, 1, 1, 1, 1, 0]
+// const DOLLAR_LEFT = [0, 0, 0, 1, 1, 0, 0, 1, 1]
+
+// create Array that indicates if a cell is already occupied.
+// Value 0 will mean 'free', 1 - 'occupied by moving block', 2 - 'occupied by previous block(s)'.
+let occupiedArray = []
+for (let i = 0; i < 25; i++) {
+  let clipboard = []
+  for (let j = 0; j < 10; j++) {
+    clipboard.push(0)
+    // this section is temporary for development purposes. Delete later
+    let tempCell = document.getElementById(getID(i, j))
+    tempCell.innerHTML = '0'
+    tempCell.style.display = 'flex'
+    tempCell.style.alignItems = 'center'
+    tempCell.style.justifyContent = 'center'
+    tempCell.style.color = 'red'
+    tempCell.style.fontWeight = '700'
+    tempCell.style.fontSize = '12px'
+    tempCell.style.width = '40px'
+    tempCell.style.height = '40px'
+    // end of temporary section
+  }
+  occupiedArray.push(clipboard)
+}
+
+// function to get an ID of a cell
+function getID(r, c) {
+  return r.toString() + '-' + c.toString()
+}
+
+// this section is temporary for development purposes. Delete later
+function temporaryShowInner() {
+  for (let i = 0; i < 25; i++) {
+    for (let j = 0; j < 10; j++) {
+      tempCell = document.getElementById(getID(i, j))
+      tempCell.innerHTML = occupiedArray[i][j]
+    }
+  }
+}
+// end of temporary section
+
+// temporary. newElem selection should be randomised
+let newElem = LINE_TWO
+
+// initial place of block on gamefield
+let currentBlock = new Block(newElem)
+render(currentBlock)
+
+// function to check if able to render.
+function checkBeforeRendering(plannedBlock) {
+  for (let i = 0; i < plannedBlock.idArray.length; i++) {
+    if (
+      occupiedArray[plannedBlock.idArray[i][0]][plannedBlock.idArray[i][1]] ===
+      2
+    ) {
+      return false
+    }
+  }
+  return true
+}
+
+// function to render (re-render) block on gamefield
+function render(block) {
+  let cell
+  for (let i = 0; i < block.idArray.length; i++) {
+    cell = document.getElementById(
+      getID(block.idArray[i][0], block.idArray[i][1])
+    )
+    if (block.colors[i]) {
+      cell.style.backgroundColor = BLOCK_COLOR
+    } else if (occupiedArray[block.idArray[i][0]][block.idArray[i][1]] !== 2) {
+      cell.style.backgroundColor = STANDARD_COLOR
+    }
+  }
+  // this section is temporary for development purposes. Delete later
+  temporaryShowInner()
+  // end of temporary section
+}
+
+// function to mark cells on gamefield as 'feee', 'occupied' or 'occupied forever' on move of a Block
+function markOccupiedOrFree(block) {
+  for (let i = 0; i < block.idArray.length; i++) {
+    if (occupiedArray[block.idArray[i][0]][block.idArray[i][1]] !== 2) {
+      occupiedArray[block.idArray[i][0]][block.idArray[i][1]] = block.colors[i]
+    }
+  }
+  // this section is temporary for development purposes. Delete later
+  temporaryShowInner()
+  // end of temporary section
+}
+
+// rotate block clockwise
+function rotateright() {
+  let copy = currentBlock.colors
+  if (currentBlock.idArray.length === 9) {
+    currentBlock.colors = [
+      copy[6],
+      copy[3],
+      copy[0],
+      copy[7],
+      copy[4],
+      copy[1],
+      copy[8],
+      copy[5],
+      copy[2],
+    ]
+  } else if (currentBlock.idArray.length === 4) {
+    currentBlock.colors = [copy[2], copy[0], copy[3], copy[1]]
+  }
+  if (!checkBeforeRendering(currentBlock)) {
+    currentBlock.colors = copy
+    return
+  }
+  render(currentBlock)
+  markOccupiedOrFree(currentBlock)
+  // console.log(occupiedArray);
+}
+
+// rotate block counter-clockwise
+function rotateleft() {
+  let copy = currentBlock.colors
+  if (currentBlock.idArray.length === 9) {
+    currentBlock.colors = [
+      copy[2],
+      copy[5],
+      copy[8],
+      copy[1],
+      copy[4],
+      copy[7],
+      copy[0],
+      copy[3],
+      copy[6],
+    ]
+  } else if (currentBlock.idArray.length === 4) {
+    currentBlock.colors = [copy[1], copy[3], copy[0], copy[2]]
+  }
+  if (!checkBeforeRendering(currentBlock)) {
+    currentBlock.colors = copy
+    return
+  }
+  render(currentBlock)
+  markOccupiedOrFree(currentBlock)
+  // console.log(occupiedArray);
+}
+/*
+// moving block left
+function moveleft() {
+  prepareToShiftHorizontal(-1)
+  render(currentBlock)
+}
+
+// moving block right
+function moveleft() {
+  prepareToShiftHorizontal(1)
+  render(currentBlock)
+}
+
+function prepareToShiftHorizontal(direction) {
+  let copyBlock = currentBlock
+  currentBlock.colors = copyBlock.colors
+  for (let i = 0; i < currentBlock.idArray.length; i++) {
+    currentBlock.idArray[i][0] += direction
+    if (
+      occupiedArray[currentBlock.idArray[i][0] - 1 + direction][
+        currentBlock.idArray[i][1] - 1
+      ] === 2 ||
+      currentBlock.idArray[i][0] < 1 ||
+      currentBlock.idArray[i][0] > 10
+    ) {
+      currentBlock = copyBlock
+      return
+    }
+  }
+  // make good those cells from which the current block departs
+  let beingLeftBehind = []
+  if (direction < 0 && copyBlock.idArray[2][1] < 10) {
+    beingLeftBehind = [
+      copyBlock.idArray[2],
+      copyBlock.idArray[5],
+      copyBlock.idArray[8],
+    ]
+  } else if (direction > 0 && copyBlock.idArray[0][1] > 1) {
+    beingLeftBehind = [
+      copyBlock.idArray[0],
+      copyBlock.idArray[3],
+      copyBlock.idArray[6],
+    ]
+  }
+  for (let j = 0; j < 3; j++) {
+    if (occupiedArray[(beingLeftBehind[0] - 1, beingLeftBehind[1] - 1)] !== 2) {
+      occupiedArray[(beingLeftBehind[0] - 1, beingLeftBehind[1] - 1)] = 0
+      let id =
+        beingLeftBehind[0].toString() + '-' + beingLeftBehind[1].toString()
+      console.log(beingLeftBehind)
+      document.getElementById(id).style.backgroundColor = STANDARD_COLOR
+    }
+  }
+} */
